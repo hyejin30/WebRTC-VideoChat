312df0523cfdf84486545cd640c6ee75cbd5af85
hyejin30 <tyui3185@gmail.com>
2022-03-30 20:03:18 +0900

    Fix : theme color 수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,16 @@
 import {DefaultTheme} from 'styled-components/native';
+import {Colors} from 'react-native-paper';
 
 const theme: DefaultTheme = {
+  color: {
+    primary: '#065e85',
+    black: '#000',
+    white: '#fff',
+    grey100: Colors.grey100,
+    grey200: Colors.grey200,
+    red100: Colors.red100,
+    red200: Colors.red200,
+  },
   fontSize: {
     header: '18px',
     body: '12px',
