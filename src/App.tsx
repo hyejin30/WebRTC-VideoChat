cc41d53abb83932254f26ab40d2e2a5d7c470dab
hyejin30 <tyui3185@gmail.com>
2022-03-30 19:50:17 +0900

    Add : Theme 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {ThemeProvider} from 'styled-components/native';
 import Entry from './pages/Entry';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import Main from './pages/Main';
 import VideoChat from './pages/VideoChat';
+import theme from './styles/theme';
 
 const Stack = createNativeStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Entry"
-        screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Entry" component={Entry} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="Main" component={Main} />
-        <Stack.Screen name="VideoChat" component={VideoChat} />
-      </Stack.Navigator>
+      <ThemeProvider theme={theme}>
+        <Stack.Navigator
+          initialRouteName="Entry"
+          screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Entry" component={Entry} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="SignIn" component={SignIn} />
+          <Stack.Screen name="Main" component={Main} />
+          <Stack.Screen name="VideoChat" component={VideoChat} />
+        </Stack.Navigator>
+      </ThemeProvider>
     </NavigationContainer>
   );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+import {DefaultTheme} from 'styled-components/native';
+
+const theme: DefaultTheme = {
+  fontSize: {
+    header: '18px',
+    body: '12px',
+  },
+};
+
+export default theme;
