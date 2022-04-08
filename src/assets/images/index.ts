714bb6c90fad9f392f214198d55cadf5a143edf7
hyejin30 <tyui3185@gmail.com>
2022-04-08 17:39:57 +0900

    Add : VideoChat UI 1차 완성, localStream 영상 출력

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -3,3 +3,6 @@ export {default as icEyeOff} from './ic-eye-off.png';
 export {default as icEyeOn} from './ic-eye-on.png';
 export {default as icLogo} from './logo_o.png';
 export {default as imgMain} from './illu-01.png';
+export {default as btnAudioOff} from './btn-audio-off.png';
+export {default as btnVideoOn} from './btn-video-on.png';
+export {default as btnCameraReverse} from './btn-camera-reverse.png';
